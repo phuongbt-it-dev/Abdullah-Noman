583b3d8983e89ad0082ae7454b2e7b4c9caaf2f2
Nguyễn Hoàng Anh Toàn <nguyenha.toan121199@gmail.com>
2018-10-25 14:32:36 +0700

    update js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,6 +5,8 @@ $(document).ready(function() {
         items: 1,
         nav: false,
         dots: true,
+        loop: true,
+        autoplay: true,
         navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
         responsive: {
             0: {
